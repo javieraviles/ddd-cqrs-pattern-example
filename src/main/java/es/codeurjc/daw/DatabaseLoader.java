8916f6b29e21681764b16df76c0265084acafaa1
Javier Aviles <avileslopez.javier@gmail.com>
2020-02-09 19:43:29 +0100

    Database loader for clients

---
+++ src/main/java/es/codeurjc/daw/DatabaseLoader.java
@@ -0,0 +1,25 @@
+package es.codeurjc.daw;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Controller;
+
+import es.codeurjc.daw.model.Client;
+import es.codeurjc.daw.repository.ClientRepository;
+
+@Controller
+public class DatabaseLoader implements CommandLineRunner {
+
+	@Autowired
+	ClientRepository clientRepository;
+
+	@Override
+	public void run(String... args) {
+
+		final Client client = new Client("Maria Frutos", "mfrutos@gmail.com", "(+34)679-434-111",
+				LocalDate.of(1984, 10, 10));
+		clientRepository.save(client);
+	}
+}
